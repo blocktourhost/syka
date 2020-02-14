37760a59ffd5acddf8676ee2203697f48d634adb
blocktourhost <60049299+blocktourhost@users.noreply.github.com>
2020-02-14 16:47:36 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1581691463,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"channel.html",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js",
+		"fapi5.js",
+		"Photon-Javascript_SDK.min.js",
+		"adapter.js",
+		"cordova.oauth2.js",
+		"xd_connection.js"
+	]
+}
